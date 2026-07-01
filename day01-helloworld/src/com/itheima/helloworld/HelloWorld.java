79c1e4191cde734de40b7b6a53366e05816f456d
MoeYe <2536428866@qq.com>
2026-07-01 20:22:53 +0800

    HelloWorld

--- day01-helloworld/src/com/itheima/helloworld/HelloWorld.java
+++ day01-helloworld/src/com/itheima/helloworld/HelloWorld.java
@@ -3,5 +3,14 @@ package com.itheima.helloworld;
 public class HelloWorld {
     static void main() {
         System.out.println("Hello World!");
+        printTen();
     }
+    //打印十行Hello World
+    static void printTen() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World!");
+        }
+    }
+
 }
+//学过C语言，不想写基础语法
